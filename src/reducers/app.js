5e5ea0f796a84bab68d042ac85854981b7c38431
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2017-07-07 14:56:48 +0900

    organize app alert dialog

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -10,11 +10,6 @@ const app = (state = initState, action) => {
         alertOpen: true,
         alertMessage: action.alertMessage,
       })
-    case 'FACE_DETECT_NONE':
-      return Object.assign({}, state, {
-        alertOpen: true,
-        alertMessage: '顔情報を認識できません。違う写真をアップロードしてください。',
-      })
     case 'APP_CLOSE_ALERT':
       return Object.assign({}, state, {
         alertOpen: false,
--- src/reducers/signUp.js
+++ src/reducers/signUp.js
@@ -5,35 +5,35 @@ const initState = {
 }
 
 const signUp = (state = initState, action) => {
-   switch (action.type) {
-     case 'SAVE_CREDIT_CARD':
-       return Object.assign({}, state, {
-         stepIndex: 1,
-         dispCardNo: action.dispCardNo,
-       })
-     case 'SAVE_FACE_PHOTO':
-       return Object.assign({}, state, {
-         stepIndex: 2,
-         dispPhotoUrl: action.dispPhotoUrl,
-       })
-     case 'RESET_CREDIT_CARD':
-       return Object.assign({}, state, {
-         stepIndex: 0,
-         dispCardNo: '',
-       })
-     case 'RESET_FACE_PHOTO':
-       return Object.assign({}, state, {
-         stepIndex: 1,
-         dispPhotoUrl: '',
-       })
-     case 'SAVE_CREDIT_CARD_ERROR':
-       return Object.assign({}, state, {
-       })
-     case 'SAVE_FACE_PHOTO_ERROR':
-       return Object.assign({}, state, {
-         stepIndex: 1,
-         dispPhotoUrl: '',
-       })
+  switch (action.type) {
+    case 'SAVE_CREDIT_CARD':
+      return Object.assign({}, state, {
+        stepIndex: 1,
+        dispCardNo: action.dispCardNo,
+      })
+    case 'SAVE_FACE_PHOTO':
+      return Object.assign({}, state, {
+        stepIndex: 2,
+        dispPhotoUrl: action.dispPhotoUrl,
+      })
+    case 'RESET_CREDIT_CARD':
+      return Object.assign({}, state, {
+        stepIndex: 0,
+        dispCardNo: '',
+      })
+    case 'RESET_FACE_PHOTO':
+      return Object.assign({}, state, {
+        stepIndex: 1,
+        dispPhotoUrl: '',
+      })
+    case 'SAVE_CREDIT_CARD_ERROR':
+      return Object.assign({}, state, {
+      })
+    case 'SAVE_FACE_PHOTO_ERROR':
+      return Object.assign({}, state, {
+        stepIndex: 1,
+        dispPhotoUrl: '',
+      })
     default:
       return state
   }
